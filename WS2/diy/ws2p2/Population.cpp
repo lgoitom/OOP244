b263d9186a7c8c17f1be56c5b80ccc235f9c8fc0
Luwam <lgoitom-worre@myseneca.ca>
2023-09-21 07:01:49 -0300

    ws2p2 incomplete

---
+++ WS2/diy/ws2p2/Population.cpp
@@ -0,0 +1,41 @@
+/***********************************************************************
+// OOP244 Workshop #2 DIY (part 2):
+//
+// File  Population.cpp
+// Version 1.0
+// Author   Fardad Soleimanloo
+// Description
+//   To completed by students
+// Revision History
+// -----------------------------------------------------------
+// Name            Date            Reason
+***********************************************************************/
+#include "Population.h"
+#include <iostream>
+#include <cstring>
+namespace sdds {
+    bool startsWith(const char* cstring, const char* subString) {
+        return std::strstr(cstring, subString) == cstring;
+    }
+    bool getPostalCode(char* postal_code_prefix) {
+        std::cout << "Population report" << endl;
+        std::cout << "-----------------" << endl;
+        std::cout << "Enter postal code:" << endl;
+        std::cout << "> " << endl;
+        std::cin >> postal_code_prefix;
+
+        if (*postal_code_prefix = '!') {
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    }
+    bool load(const char* filename) {
+
+    }
+
+
+
+}
---
+++ WS2/diy/ws2p2/Population.h
@@ -0,0 +1,25 @@
+/***********************************************************************
+// OOP244 Workshop #2 DIY (part 2):
+//
+// File  Population.h
+// Version 1.0
+// Author   Fardad Soleimanloo
+// Description
+//   To completed by students
+// Revision History
+// -----------------------------------------------------------
+// Name            Date            Reason
+***********************************************************************/#ifndef SDDS_POPULATION_H_
+#define SDDS_POPULATION_H_
+#include <iostream>
+
+using namespace std;
+namespace sdds {
+	// returns true of the cstring starts with subString
+	bool startsWith(const char* cString, const char* subString);
+	bool getPostalCode(char* postal_code_prefix);
+	bool load(const char* filename);
+
+
+}
+#endif // SDDS_POPULATION_H_
